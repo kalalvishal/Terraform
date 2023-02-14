abc0146f6d3274aa135934085391cf6517fa2ee2
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-02-14 12:13:30 -0600

    Added Backend Details.

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -13,9 +13,10 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "this" {
-  for_each            = local.subnets
-  name                = each.value["name"]
-  address_prefixes    = each.value["address_prefix"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  for_each             = local.subnets
+  name                 = each.value["name"]
+  address_prefixes     = each.value["address_prefix"]
+  location             = azurerm_resource_group.this.location
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
 }
