1df715274eb68ec6afa2c4f64190267448cf9862
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-02-14 12:11:05 -0600

    Added Backend Details.

--- src/terraform/_backend.tf
+++ src/terraform/_backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "RG-EastUS-TFState"
+    storage_account_name = "strgacttfstate1508748498"
+    container_name       = "tfstate"
+    key                  = "Demo/GithubDemo.tfstate"
+  }
 }
